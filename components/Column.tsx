f0387be44fe83eb5beac527078bb995c3eff63e6
Selmen Rayen <selmanerayene95@gmail.com>
2023-09-21 19:18:01 +0100

    add search feature

--- components/Column.tsx
+++ components/Column.tsx
@@ -2,6 +2,7 @@ import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
+import { useBoardStore } from "@/store/BoardStore";
 
 type Props = {
   id: TypedColumn;
@@ -16,6 +17,7 @@ const idToColumnText: { [key in TypedColumn]: string } = {
 };
 
 export default function Column({ id, todos, index }: Props) {
+  const [searchString] = useBoardStore((state) => [state.searchString]);
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -37,29 +39,47 @@ export default function Column({ id, todos, index }: Props) {
                 <h2 className="flex justify-between font-bold text-xl p-2 ">
                   {idToColumnText[id]}
                   <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
-                    {todos.length}
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title
+                            .toLowerCase()
+                            .includes(searchString.toLowerCase())
+                        ).length}
                   </span>
                 </h2>
 
                 <div className="space-y-2">
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.$id}
-                      draggableId={todo.$id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <TodoCard
-                          todo={todo}
-                          index={index}
-                          id={id}
-                          innerRef={provided.innerRef}
-                          draggableProps={provided.draggableProps}
-                          dragHandleProps={provided.dragHandleProps}
-                        />
-                      )}
-                    </Draggable>
-                  ))}
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString &&
+                      !todo.title
+                        .toLowerCase()
+                        .includes(searchString.toLowerCase())
+                    )
+                      return;
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => {
+                          return (
+                            <TodoCard
+                              todo={todo}
+                              index={index}
+                              id={id}
+                              innerRef={provided.innerRef}
+                              draggableProps={provided.draggableProps}
+                              dragHandleProps={provided.dragHandleProps}
+                            />
+                          );
+                        }}
+                      </Draggable>
+                    );
+                  })}
+
                   {provided.placeholder}
 
                   <div className="flex items-end justify-end p-2">
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -7,6 +7,9 @@ interface BoardState {
   getBoard: () => void;
   setBoardState: (board: Board) => void;
   updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
+
+  searchString: string;
+  setSearchString: (searchString: string) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -25,5 +28,9 @@ export const useBoardStore = create<BoardState>((set) => ({
       todo.$id,
       { title: todo.title, status: columnId }
     );
+  },
+  searchString: "",
+  setSearchString: (searchString) => {
+    set((state) => ({ searchString }));
   },
 }));
